dfa7f921705b26e7104ffc2d4cde81fe1ed0f5e1
Cameron Dykes <yellow5c@gmail.com>
2019-07-16 11:49:50 -0500

    Add documentation for current code

--- lib/sm_sms_campaign_webhook.rb
+++ lib/sm_sms_campaign_webhook.rb
@@ -2,6 +2,8 @@
 
 require "sm_sms_campaign_webhook/version"
 
+# Namespace for SMS campaign webhook.
 module SmSmsCampaignWebhook
+  # General base error type for custom errors.
   class Error < StandardError; end
 end
