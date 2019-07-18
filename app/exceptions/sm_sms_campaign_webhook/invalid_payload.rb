28afe9dda1a3b646a3a7d172b7bc43dfa7ba9853
Cameron Dykes <yellow5c@gmail.com>
2019-07-18 17:29:02 -0500

    Add InvalidPayloadValue custom error type

--- app/exceptions/sm_sms_campaign_webhook/invalid_payload.rb
+++ app/exceptions/sm_sms_campaign_webhook/invalid_payload.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module SmSmsCampaignWebhook
-  # Error type for invalid data received by the SMS webhook.
+  # Error type for invalid payload schema received by the SMS webhook.
   class InvalidPayload < Error; end
 end
