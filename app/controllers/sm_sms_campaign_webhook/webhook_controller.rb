3b7e2bad8788954b4f24acfba9a6841b69dcc734
Cameron Dykes <yellow5c@gmail.com>
2019-07-19 15:08:56 -0500

    Update POST /api/webhook to deserialize JSON and model the data

--- app/controllers/sm_sms_campaign_webhook/webhook_controller.rb
+++ app/controllers/sm_sms_campaign_webhook/webhook_controller.rb
@@ -6,8 +6,19 @@ module SmSmsCampaignWebhook
   # API webhook for POST requests from SMS campaign service.
   class WebhookController < ApplicationController
     # POST /api/webhook
+    # @see PayloadOperation.cast
     def create
+      # Deserialize the payload.
+      payload = JSON.parse(request.body.read)
+      logger.debug "#{self.class} - Payload: #{payload.inspect}"
+
+      # Cast the payload data with the appropriate data model.
+      PayloadOperation.cast(payload: payload)
+
       head :no_content
+    rescue JSON::ParserError => e
+      logger.warn "#{self.class} - Bad Request: #{e.class} - #{e}"
+      head :bad_request
     end
   end
 end
--- spec/requests/api_webhook_spec.rb
+++ spec/requests/api_webhook_spec.rb
@@ -1,6 +1,73 @@
+require_relative "../support/helpers/sms_campaign_payload"
+
 RSpec.describe "API webhook", type: :request do
-  it "responds wtih success" do
-    post "/sms_campaign/api/webhook"
-    expect(response).to have_http_status(:no_content)
+  include Helpers::SmsCampaignPayload
+
+  let(:headers) do
+    {
+      "Content-Type" => "application/json",
+      "Accept" => "application/json"
+    }
+  end
+
+  context "without any payload" do
+    it "responds with bad request" do
+      post "/sms_campaign/api/webhook", headers: headers
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  context "without json payload" do
+    let(:headers) do
+      {
+        "Accept" => "*/*"
+      }
+    end
+    let(:payload) do
+      {q: "not json"}
+    end
+
+    it "responds with bad request" do
+      post "/sms_campaign/api/webhook", headers: headers, params: payload
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  context "with unsupported payload" do
+    let(:payload) do
+      unsupported_event_json
+    end
+
+    it "models the payload" do
+      payload_hash = JSON.parse(payload)
+      expect(
+        SmSmsCampaignWebhook::PayloadOperation
+      ).to receive(:cast).with(payload: payload_hash)
+      post "/sms_campaign/api/webhook", headers: headers, params: payload
+    end
+
+    it "responds with success" do
+      post "/sms_campaign/api/webhook", headers: headers, params: payload
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+
+  context "with campaign engagement payload" do
+    let(:payload) do
+      campaign_engagement_json
+    end
+
+    it "models the payload" do
+      payload_hash = JSON.parse(payload)
+      expect(
+        SmSmsCampaignWebhook::PayloadOperation
+      ).to receive(:cast).with(payload: payload_hash)
+      post "/sms_campaign/api/webhook", headers: headers, params: payload
+    end
+
+    it "responds with success" do
+      post "/sms_campaign/api/webhook", headers: headers, params: payload
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
