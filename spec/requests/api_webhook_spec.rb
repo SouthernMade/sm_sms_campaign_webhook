df6fe33b8c3d19be844c9810eb1ec1381c4fb4f0
Cameron Dykes <yellow5c@gmail.com>
2019-07-22 14:48:49 -0500

    Refactor POST /api/webhook to dispatch payload async

--- spec/requests/api_webhook_spec.rb
+++ spec/requests/api_webhook_spec.rb
@@ -34,11 +34,11 @@ RSpec.describe "API webhook", type: :request do
       unsupported_event_json
     end
 
-    it "models the payload" do
+    it "schedules job to dispatch the payload" do
       payload_hash = JSON.parse(payload)
       expect(
-        SmSmsCampaignWebhook::PayloadOperation
-      ).to receive(:cast).with(payload: payload_hash)
+        SmSmsCampaignWebhook::DispatchPayloadJob
+      ).to receive(:perform_later).with(payload_hash)
       post "/sms_campaign/api/webhook", headers: headers, params: payload
     end
 
@@ -53,11 +53,11 @@ RSpec.describe "API webhook", type: :request do
       campaign_engagement_json
     end
 
-    it "models the payload" do
+    it "schedules job to dispatch the payload" do
       payload_hash = JSON.parse(payload)
       expect(
-        SmSmsCampaignWebhook::PayloadOperation
-      ).to receive(:cast).with(payload: payload_hash)
+        SmSmsCampaignWebhook::DispatchPayloadJob
+      ).to receive(:perform_later).with(payload_hash)
       post "/sms_campaign/api/webhook", headers: headers, params: payload
     end
 
