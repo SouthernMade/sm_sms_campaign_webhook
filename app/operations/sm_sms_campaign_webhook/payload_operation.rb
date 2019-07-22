67347652f310f40745513d925000057fb3d12f1d
Cameron Dykes <yellow5c@gmail.com>
2019-07-22 17:08:48 -0500

    Update PayloadOperation to include log entries

--- app/operations/sm_sms_campaign_webhook/payload_operation.rb
+++ app/operations/sm_sms_campaign_webhook/payload_operation.rb
@@ -6,12 +6,21 @@ module SmSmsCampaignWebhook
     # @param payload [Hash] Deserialized payload from SMS campaign service
     # @see ProcessCampaignEngagementJob#perform
     def self.dispatch(payload:)
+      logger.debug "#{name} - Dispatching payload: #{payload.inspect}"
+
       case payload.fetch("type", "unknown")
       when "campaign.engagement"
         ProcessCampaignEngagementJob.perform_later(payload)
       else
         # NOOP - Unsupported event type.
+        logger.warn "#{name} - Unsupported event type"
       end
     end
+
+    # @return [ActiveSupport::Logger] Abstraction of app logger
+    def self.logger
+      @logger ||= Rails.logger
+    end
+    private_class_method :logger
   end
 end
