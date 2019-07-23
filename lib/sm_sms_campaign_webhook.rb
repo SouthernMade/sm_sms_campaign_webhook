3408482a035a3d694eba8cd4db7183f2bb04ce26
Cameron Dykes <yellow5c@gmail.com>
2019-07-23 14:53:09 -0500

    Add SmSmsCampaignWebhook.processor config methods

--- lib/sm_sms_campaign_webhook.rb
+++ lib/sm_sms_campaign_webhook.rb
@@ -5,4 +5,14 @@ require "sm_sms_campaign_webhook/version"
 
 # Namespace for SMS campaign webhook.
 module SmSmsCampaignWebhook
+  # @return [Processable] SMS campaign payload processor used by operations
+  def self.processor
+    @processor ||= DefaultProcessor
+  end
+
+  # @param processor [Processable] Custom SMS campaign payload processor
+  # @see Processable
+  def self.processor=(processor)
+    @processor = processor
+  end
 end
--- spec/sm_sms_campaign_webhook_spec.rb
+++ spec/sm_sms_campaign_webhook_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe SmSmsCampaignWebhook do
       expect(described_class::VERSION).to_not be_nil
     end
   end
+
+  describe ".processor" do
+    it "defaults to default processor" do
+      expect(
+        described_class.processor
+      ).to eq(SmSmsCampaignWebhook::DefaultProcessor)
+    end
+  end
+
+  describe ".processor=" do
+    let(:processor_klass) do
+      class MockProcessor
+        include SmSmsCampaignWebhook::Processable
+      end
+      MockProcessor
+    end
+
+    before do
+      @original_processor = described_class.processor
+    end
+
+    after do
+      described_class.instance_variable_set(:@processor, @original_processor)
+    end
+
+    it "updates process config" do
+      expect do
+        described_class.processor = processor_klass
+      end.to change(described_class, :processor).to(processor_klass)
+    end
+  end
 end
