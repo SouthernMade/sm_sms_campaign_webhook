6e885d29d345aad89bd89ccc9d2d880fe5a5c2f0
Cameron Dykes <yellow5c@gmail.com>
2019-07-24 14:06:06 -0500

    Add install generator copying initializer

---
+++ lib/generators/sm_sms_campaign_webhook/install/install_generator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails/generators/base"
+
+module SmSmsCampaignWebhook
+  # Namespace for generators provided by the gem.
+  module Generators
+    # Installs files to prep an app for SMS campaign webhook.
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("templates", __dir__)
+
+      # Copy initializer template to config/initializers
+      def copy_initializer
+        template "sm_sms_campaign_webhook.rb", "config/initializers/sm_sms_campaign_webhook.rb"
+      end
+    end
+  end
+end
